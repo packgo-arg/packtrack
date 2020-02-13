4af3761935f9dd284b3ea97b073a56d3681a75ff
Ignacio Grosso <ignaciogrosso83@gmail.com>
2020-02-13 12:34:19 -0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os, sys
 import django_heroku
 import dj_database_url
-#import dotenv
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#dotenv_file = os.path.join(BASE_DIR, ".env")
-#if os.path.isfile(dotenv_file):
-#    dotenv.load_dotenv(dotenv_file)
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -113,21 +113,21 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 #Changed DB to use POSTGRESQL. """ GENERATE NEW DB USER AND PASS FOR PROD USE """
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'packtrack',
-        'USER': 'postgres',
-        'PASSWORD': 'wa7daf4k',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'packtrack',
+#        'USER': 'postgres',
+#        'PASSWORD': 'wa7daf4k',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 #ADDED DB LINK TO HEROKU POSTGRESQL
 
-#DATABASES = {}
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
@@ -171,5 +171,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 sys.path.append(os.path.join(PROJECT_ROOT, 'tasks/lib'))
-#django_heroku.settings(locals())
-#del DATABASES['default']['OPTIONS']['sslmode']
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
