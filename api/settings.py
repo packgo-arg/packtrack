c595efd6dead0e1106745e2519a2597904d2c229
Ignacio Grosso <ignaciogrosso83@gmail.com>
2020-04-06 12:34:15 -0300

    Fix error in SSL mode (try #2).

--- api/settings.py
+++ api/settings.py
@@ -18,11 +18,11 @@ from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ENV=False
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
-
+    ENV=True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
