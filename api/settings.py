34bb4143586f7c681d0783c46755dadca7021cec
Ignacio Grosso <ignaciogrosso83@gmail.com>
2020-05-14 18:04:13 -0300

    Correction in settings.py

--- api/settings.py
+++ api/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
-else:
-    django_heroku.settings(locals())
+
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
