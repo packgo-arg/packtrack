1725a6647623af6b07aaac941ebe8a4da73f0464
Ignacio Grosso <ignaciogrosso83@gmail.com>
2020-05-14 17:58:50 -0300

    change in settings.py

--- api/settings.py
+++ api/settings.py
@@ -15,6 +15,7 @@ import sys
 import dj_database_url
 import dotenv
 from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +23,6 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 else:
-    import django_heroku
     django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
